e710dd5e84e36921e1822d6ff5c419b2e561fd6f
Egor Popov <ijfgio@yandex.ru>
2023-12-28 19:24:52 +0300

    Javka

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         float balance = 200F; //остаток на счете
-        float added = 2000F; // пополнение счета
+        float added = 3000F; // пополнение счета
         int bonus = 100;
         float newBalance = 0;
         if (added >= 1000) {
