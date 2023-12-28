2ae694d37105af531122d88eb642b4ae73bc18d4
Egor Popov <ijfgio@yandex.ru>
2023-12-28 17:50:31 +0300

    gitegnore

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        float balance = 200F; //остаток на счете
+        float added = 2000F; // пополнение счета
+        int bonus = 100;
+        float newBalance = 0;
+        if (added >= 1000) {
+            bonus = (int) added / 100 ;
+            newBalance = balance + added + bonus;
+            System.out.println("Вам начислено " + bonus + " бонусных рублей.");
+            System.out.println("На вашем балансе " + newBalance + " рублей.");
+        } else {
+            newBalance = balance + added;
+            System.out.println("На вашем балансе " + newBalance + " рублей.");
+
+
+        }
+    }
+}
