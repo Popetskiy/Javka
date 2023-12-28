78f5bee64462e59d4769469cf07d678c857eab22
Egor Popov <ijfgio@yandex.ru>
2023-12-28 19:34:31 +0300

    Javka

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         float balance = 200F; //остаток на счете
-        float added = 4000F; // пополнение счета
+        float added = 3000F; // пополнение счета
         int bonus = 100;
         float newBalance = 0;
         if (added >= 1000) {
-            bonus = (int) added / 100 ;
+            bonus = (int) added / 100;
             newBalance = balance + added + bonus;
             System.out.println("Вам начислено " + bonus + " бонусных рублей.");
             System.out.println("На вашем балансе " + newBalance + " рублей.");
